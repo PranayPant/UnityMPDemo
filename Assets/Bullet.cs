0f3cee6b555726ad798a975271473ba21b8f6f92
PranayPant <ppantg1@gmail.com>
2017-09-16 17:31:13 -0500

    first push

---
+++ Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+
+    [SyncVar]
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        Debug.Log(currentHealth);
+        if (currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+            RpcRespawn();
+        }
+
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // move back to zero location
+            transform.position = Vector3.zero;
+        }
+    }
+
+
+}
